3db5f14bafc87f7eb503874c0061fe5fc43ea895
Chinmay Moharir <chinmay.moharir@gmail.com>
2017-03-06 23:46:07 -0800

    Update tests.js

--- scripts/tests.js
+++ scripts/tests.js
@@ -1,52 +1,56 @@
-var App = window.App || {};
+var ds = new App.DataStore();
 
- QUnit.test("DataStore Add", function(assert) {
-     var ds = new App.DataStore();
-     ds.add('m@bond.com', 'tea');
-     ds.add('james@bond.com', 'eshpressho');
-     ds.getAll();
-     assert.ok(ds.getAll(), "Passed!");
- });
+QUnit.test('add ok test', function(assert) {
+    assert.equal(Object.keys(ds.getAll()).length, 0, 'Passed!');
+    ds.add('m@bond.com', 'tea');
+    assert.equal(Object.keys(ds.getAll()).length, 1, 'Passed!');
+    assert.ok(Object.keys(ds.getAll()).length, 1, 'Passed!');
+});
 
- QUnit.test("DataStore Remove", function(assert) {
-     var ds = new App.DataStore();
-     ds.add('james@bond.com', 'eshpressho');
-     ds.remove('james@bond.com');
-     assert.ok(ds.get('james@bond.com') == undefined, "Passed!");
- });
+QUnit.test('addAll ok test', function(assert) {
+    ds.add('m@bond.com', 'tea');
+    ds.add('james@bond.com', 'eshpressho');
 
- QUnit.test("DataStore Get", function(assert) {
-     var ds = new App.DataStore();
-     ds.add('m@bond.com', 'tea');
-     ds.add('james@bond.com', 'eshpressho');
-     assert.ok(ds.get('james@bond.com') == "eshpressho", "Passed!");
- });
+    var obj = {
+        'm@bond.com': 'tea',
+        'james@bond.com': 'eshpressho'
+    };
+    assert.deepEqual(ds.getAll(), obj, 'Passed!');
+    assert.equal(Object.keys(ds.getAll()).length, 2, 'Passed!');
+    assert.ok(Object.keys(ds.getAll()).length, 2, 'Passed!');
+});
 
- QUnit.test("Create Orders", function(assert) {
-     window.myTruck.createOrder({
-         emailAddress: 'me@goldfinger.com',
-         coffee: 'double mocha'
-     });
-     var orderInfo = window.myTruck.db.get('me@goldfinger.com');
-     assert.ok(orderInfo.coffee == 'double mocha', "Passed!");
- });
+QUnit.test('remove ok test', function(assert) {
+    ds.remove('james@bond.com');
+    assert.ok(ds.data, ds.getAll(), 'Passed!');
+    assert.equal(ds.data, ds.getAll(), 'Passed!');
+});
 
- QUnit.test("Fill Order", function(assert) {
-     window.myTruck.createOrder({
-         emailAddress: 'me@goldfinger.com',
-         coffee: 'double mocha'
-     });
-     window.myTruck.deliverOrder('me@goldfinger.com');
-     var orderInfo = window.myTruck.db.get('me@goldfinger.com');
-     assert.ok(orderInfo == undefined, "Passed!");
- })
+QUnit.test('get ok test', function(assert) {
+    assert.ok(ds.get('m@bond.com'), 'tea', 'Passed!');
+    assert.equal(ds.get('m@bond.com'), 'tea', 'Passed!');
+    assert.strictEqual(ds.get('m@bond.com'), 'tea', 'Passed!');
+});
 
- QUnit.test("Print Orders", function(assert) {
-     window.myTruck.createOrder({
-         emailAddress: 'me@goldfinger.com',
-         coffee: 'double mocha'
-     });
-     var orders = window.myTruck.printOrders();
-     // Orders are displayed on console
-     assert.ok(true, "Passed!");
- })
+// Additional functions QUnit test functions for Truck DataStore
+QUnit.test('createOrderTest ok test', function(assert) {
+    var order = {
+        emailAddress: 'dd@gmail.com',
+        coffee: 'decaf'
+    };
+    assert.deepEqual(myTruck.createOrderTest(order), order, 'Passed!');
+});
+
+QUnit.test('printOrdersTest ok test', function(assert) {
+    assert.equal(myTruck.printOrdersTest(), Object.keys(myTruck.db.data).length, 'Passed!');
+});
+
+QUnit.test('printOrdersTest ok test', function(assert) {
+    assert.equal(myTruck.deliverOrderTest('dd@gmail.com'), undefined, 'Passed!');
+});
+
+
+/*
+  What problem do you run into when you attempt to convert the code in Figure 8.32 into QUnit tests for Truck?
+We had no return type in the functions of assignments, thus they were returning nothing and we were receiving "undefined" on console and on QUnit due to which test cases were failing.
+*/
